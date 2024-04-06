b100bb22dec111ac23f5cc7195265354e0f2acc0
torarnehave <torarnehave@gmail.com>
2024-04-06 18:59:18 +0200

    Delete public/index.html and add server rendering for index page
    * public/index.html is deleted
    * Server is now rendering and sending index.html on GET requests to root path ('/')

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 // Serve files from the public directory
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 // Additional middleware for parsing request bodies, if needed
 app.use(express.json()); // For JSON
@@ -15,6 +15,11 @@ app.post('/submit', (req, res) => {
     console.log('Data received:', data);
     res.send(`Received your data!`);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
